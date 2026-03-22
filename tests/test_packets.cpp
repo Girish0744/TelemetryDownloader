2a320b8f5130b590e09c517af42451500ab52ec7
Girish0744 <girishbhuteja07@gmail.com>
2026-03-22 17:29:12 -0400

    Add packet test cases and test skeleton

---
+++ tests/test_packets.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../common/packet.h"
+
+int main()
+{
+    Packet p;
+    p.packetType = HELLO;
+    p.sequenceNumber = 1;
+    p.payloadLength = 0;
+
+    if (p.packetType == HELLO && p.sequenceNumber == 1 && p.payloadLength == 0)
+    {
+        std::cout << "Packet test passed\n";
+    }
+    else
+    {
+        std::cout << "Packet test failed\n";
+    }
+
+    return 0;
+}
