f0b91088b771aae846182740e7463eb5ae0c60c5
RudraPatelhere <Rpatel1198@conestogac.on.ca>
2026-04-03 10:34:19 -0400

    Add GET_STATUS handling and response in server

--- server/server.cpp
+++ server/server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <cstring>
 #include "../common/packet.h"
 
 #pragma comment(lib, "ws2_32.lib")
@@ -20,7 +21,6 @@ int main()
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
     if (serverSocket == INVALID_SOCKET)
     {
         std::cout << "Socket creation failed\n";
@@ -50,7 +50,6 @@ int main()
     std::cout << "Server listening on port " << PORT << std::endl;
 
     clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientSize);
-
     if (clientSocket == INVALID_SOCKET)
     {
         std::cout << "Client connection failed\n";
@@ -61,30 +60,50 @@ int main()
 
     std::cout << "Client connected successfully\n";
 
-    Packet receivedPacket{};
-    int bytesReceived = recv(clientSocket, (char*)&receivedPacket, sizeof(Packet), 0);
-
-    if (bytesReceived <= 0)
+    // ?? Handle multiple requests
+    while (true)
     {
-        std::cout << "Client disconnected\n";
-        closesocket(clientSocket);
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
+        Packet receivedPacket{};
+        int bytesReceived = recv(clientSocket, (char*)&receivedPacket, sizeof(Packet), 0);
 
-    if (receivedPacket.packetType == HELLO)
-    {
-        std::cout << "HELLO received from client\n";
+        if (bytesReceived <= 0)
+        {
+            std::cout << "Client disconnected\n";
+            break;
+        }
 
-        Packet verifyPacket{};
-        verifyPacket.packetType = VERIFY;
-        verifyPacket.sequenceNumber = 1;
-        verifyPacket.payloadLength = 0;
+        // HELLO ? VERIFY
+        if (receivedPacket.packetType == HELLO)
+        {
+            std::cout << "HELLO received from client\n";
 
-        send(clientSocket, (char*)&verifyPacket, sizeof(Packet), 0);
+            Packet verifyPacket{};
+            verifyPacket.packetType = VERIFY;
+            verifyPacket.sequenceNumber = 1;
+            verifyPacket.payloadLength = 0;
 
-        std::cout << "VERIFY sent to client\n";
+            send(clientSocket, (char*)&verifyPacket, sizeof(Packet), 0);
+
+            std::cout << "VERIFY sent to client\n";
+        }
+
+        // GET_STATUS
+        else if (receivedPacket.packetType == GET_STATUS)
+        {
+            std::cout << "GET_STATUS received\n";
+
+            Packet statusPacket{};
+            statusPacket.packetType = DATA;
+            statusPacket.sequenceNumber = 2;
+
+            const char* msg = "Telemetry Ready";
+            strcpy_s(statusPacket.payload, msg);
+            statusPacket.payloadLength = strlen(msg);
+
+            send(clientSocket, (char*)&statusPacket, sizeof(Packet), 0);
+
+            std::cout << "Status sent to client\n";
+        }
     }
 
     closesocket(clientSocket);
